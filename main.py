d5b215ebe0c4d618d6cf6c6d81d75d86d4124da7
Arturo Joya <misael.joya.mj@gmail.com>
2022-12-19 12:44:06 -0500

    Add the view to the main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from controller import Controller
 from model import MVP_Model
 from register_file import RegisterFile
+from cpu_view import TextView
 
 model = MVP_Model(register_file=RegisterFile())
-controller = Controller(model,view=None)
+view = TextView(model)
+controller = Controller(model,view)
 #file = controller.prompt.get_file()
 controller.run()#file)
 None
